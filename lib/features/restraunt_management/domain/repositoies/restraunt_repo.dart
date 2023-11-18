0ff526c897996653f759be56a6973a4e20e894b6
Muhammad Rashad <admoali123@gmail.com>
2023-11-18 21:57:51 +0200

    create  restraunt feature step one

---
+++ lib/features/restraunt_management/domain/repositoies/restraunt_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:food_delivery/shared/error/failuer.dart';
+
+import '../entities/restraunt.dart';
+
+abstract class  RestrauntRepo {
+  Future<Either<Failure,List<Restraunt>>> allRestruants();
+  Future<Either<Failure,int>> insert(Restraunt r);
+  Future<Either<Failure,int>> delete(Restraunt r);
+  Future<Either<Failure,int>> update(Restraunt r);
+ }
