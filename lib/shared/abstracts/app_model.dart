270473ceccfc042893b412f7359e8ad6f7489c38
Muhammad Rashad <admoali123@gmail.com>
2024-01-01 21:56:25 +0200

    fix app model confilect

--- lib/shared/abstracts/app_model.dart
+++ lib/shared/abstracts/app_model.dart
@@ -1,2 +1,13 @@
+ 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+@module
+abstract class AppModel {
+  @injectable
+  FirebaseAuth get auth => FirebaseAuth.instance;
+  @injectable
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 }
+ 
