3592104442eef322536fc74558140e929fc7f9f6
Muhammad Rashad <admoali123@gmail.com>
2024-01-06 18:14:32 +0200

    bind image to profile

--- lib/features/user_managment/view/bloc/profile_bloc_bloc.dart
+++ lib/features/user_managment/view/bloc/profile_bloc_bloc.dart
@@ -1,4 +1,3 @@
- 
 import 'dart:io';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,23 +8,31 @@ import 'package:food_delivery/injection.dart';
 
 part 'profile_bloc_event.dart';
 part 'profile_bloc_state.dart';
-
+// todo: Cash expetion and failures..........
 class ProfileBlocBloc extends Bloc<ProfileBlocEvent, ProfileBlocState> {
-  ProfileBlocBloc() : super(const ProfileBlocInitial(mail: null,name:null,image: null,fbID: null)) {
-    on<ProfileBlocEvent>((event, emit) {
-     });
-     on<ProfileBlocRequsit>((event, emit) async {
-      User? uu =await  getIt.get<IProfManagement>().getusrtbyfbId(event.fbId);
-       emit(  ProfileBlocLoading(name:   null,mail: null,image: null,fbID: event.fbId));
-       if(uu!=null)
-       {
-        emit(ProfileBlocLaoded(mail: uu.mail,name: uu.name,image: null,fbID:  event.fbId));
-       }
-     });
-     on<ProfileBlocUpdateImage>((event, emit) {
-      emit(state.copyWith(image: event.path.path));
-      getIt.get<IProfManagement>().setProfileImage(state.fbID!, event.path);
+  ProfileBlocBloc()
+      : super(const ProfileBlocInitial(
+            mail: null, name: null, image: null, fbID: null)) {
+    on<ProfileBlocEvent>((event, emit) {});
+    on<ProfileBlocRequsit>((event, emit) async {
+      User? uu = await getIt.get<IProfManagement>().getusrtbyfbId(event.fbId);
 
-     });
+      emit(ProfileBlocLoading(
+          name: null, mail: null, image: uu.image, fbID: event.fbId));
+      if (uu != null) {
+        emit(ProfileBlocLaoded(
+            mail: uu.mail, name: uu.name, image: uu.image, fbID: event.fbId));
+      }
+    });
+    on<ProfileBlocUpdateImage>((event, emit) async {
+      emit(state.copyWith(image: event.path.path));
+      var x = getIt.get<IProfManagement>();
+      var url = await x.setProfileImage(state.fbID!, event.path);
+
+      if (url != null) {
+        emit(state.copyWith(image: url));
+        await x.updateImagPathInUserDocViafbid(state.fbID!, url);
+      }
+    });
   }
 }
