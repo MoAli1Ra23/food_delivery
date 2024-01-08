79d1ca6216a18af4e9e9d9ef2f86f109196318b6
Muhammad Rashad <admoali123@gmail.com>
2024-01-08 09:15:06 +0200

    update menu entity and add menu items

--- lib/features/menu/doamain/entiteis/menu.dart
+++ lib/features/menu/doamain/entiteis/menu.dart
@@ -2,39 +2,45 @@
 import 'dart:convert';
 
 import 'package:equatable/equatable.dart';
-import 'package:flutter/widgets.dart';
+
+import 'menu_itames.dart';
 
 class Menu extends Equatable {
   final int id;
   final int restrauntId;
   final DateTime dateTime;
+  final List<MenuItem>? items;
 
   const Menu({
     required this.id,
     required this.restrauntId,
     required this.dateTime,
+      this.items,
   });
 
   Menu copyWith({
     int? id,
     int? restrauntId,
     DateTime? dateTime,
+    List<MenuItem>? items,
   }) {
     return Menu(
       id: id ?? this.id,
       restrauntId: restrauntId ?? this.restrauntId,
       dateTime: dateTime ?? this.dateTime,
+      items: items ?? this.items,
     );
   }
 
   @override
-  List<Object> get props => [id, restrauntId, dateTime];
+  List<Object?> get props => [id, restrauntId, dateTime, items];
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
       'restrauntId': restrauntId,
       'dateTime': dateTime.millisecondsSinceEpoch,
+      'items':items!=null? items!.map((x) => x.toMap()).toList():null,
     };
   }
 
@@ -43,10 +49,14 @@ class Menu extends Equatable {
       id: map['id'] as int,
       restrauntId: map['restrauntId'] as int,
       dateTime: DateTime.fromMillisecondsSinceEpoch(map['dateTime'] as int),
+      items: List<MenuItem>.from((map['items'] as List<int>).map<MenuItem>((x) => MenuItem.fromMap(x as Map<String,dynamic>),),),
     );
   }
 
   String toJson() => json.encode(toMap());
 
   factory Menu.fromJson(String source) => Menu.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool get stringify => true;
  }
---
+++ lib/features/menu/doamain/entiteis/menu_itames.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+class MenuItem extends Equatable {
+  final String imgUrl;
+  final String dissName;
+  final String discraption;
+  final double price;
+  final double? rate;
+  MenuItem({
+    required this.imgUrl,
+    required this.dissName,
+    required this.discraption,
+    required this.price,
+    this.rate,
+  });
+
+
+  MenuItem copyWith({
+    String? imgUrl,
+    String? dissName,
+    String? discraption,
+    double? price,
+    double? rate,
+  }) {
+    return MenuItem(
+      imgUrl: imgUrl ?? this.imgUrl,
+      dissName: dissName ?? this.dissName,
+      discraption: discraption ?? this.discraption,
+      price: price ?? this.price,
+      rate: rate ?? this.rate,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'imgUrl': imgUrl,
+      'dissName': dissName,
+      'discraption': discraption,
+      'price': price,
+      'rate': rate,
+    };
+  }
+
+  factory MenuItem.fromMap(Map<String, dynamic> map) {
+    return MenuItem(
+      imgUrl: map['imgUrl'] as String,
+      dissName: map['dissName'] as String,
+      discraption: map['discraption'] as String,
+      price: map['price'] as double,
+      rate: map['rate'] != null ? map['rate'] as double : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MenuItem.fromJson(String source) => MenuItem.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props {
+    return [
+      imgUrl,
+      dissName,
+      discraption,
+      price,
+      rate,
+    ];
+  }
+}
