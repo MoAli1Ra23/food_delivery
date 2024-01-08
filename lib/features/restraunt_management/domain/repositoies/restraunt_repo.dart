dbd049244805cde474790537604d80d1af62ff65
Muhammad Rashad <admoali123@gmail.com>
2024-01-08 09:07:37 +0200

    add getRestruntbyUserID method

--- lib/features/restraunt_management/domain/repositoies/restraunt_repo.dart
+++ lib/features/restraunt_management/domain/repositoies/restraunt_repo.dart
@@ -5,6 +5,8 @@ import '../entities/restraunt.dart';
 
 abstract class  RestrauntRepo {
   Future<Either<Failure,List<Restraunt>>> allRestruants();
+  Future<Either<Failure, Restraunt>> getRestruntbyUserID(String userid);
+
   Future<Either<Failure,int>> insert(Restraunt r);
   Future<Either<Failure,int>> delete(Restraunt r);
   Future<Either<Failure,int>> update(Restraunt r);
