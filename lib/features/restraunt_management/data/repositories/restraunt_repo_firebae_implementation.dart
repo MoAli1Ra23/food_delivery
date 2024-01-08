cb44852ef638360c4b0bdf43dff28b605ed1c3a9
Muhammad Rashad <admoali123@gmail.com>
2024-01-08 09:05:29 +0200

    set env for  it and impl insert and update methods

--- lib/features/restraunt_management/data/repositories/restraunt_repo_firebae_implementation.dart
+++ lib/features/restraunt_management/data/repositories/restraunt_repo_firebae_implementation.dart
@@ -1,12 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:food_delivery/features/restraunt_management/domain/entities/restraunt.dart';
+import 'package:food_delivery/injection.dart';
 
 import 'package:food_delivery/shared/error/failuer.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../domain/repositoies/restraunt_repo.dart';
-@LazySingleton( as:RestrauntRepo )
+
+@LazySingleton(as: RestrauntRepo,env: ['prod','debug'])
 
 class RestrauntRepoFirebaseImplementation extends RestrauntRepo {
   @override
@@ -22,15 +25,40 @@ class RestrauntRepoFirebaseImplementation extends RestrauntRepo {
   }
 
   @override
-  Future<Either<Failure, int>> insert(Restraunt r) {
-    // TODO: implement insert
-    throw UnimplementedError();
+  Future<Either<Failure, int>> insert(Restraunt r) async {
+     try {
+      FirebaseFirestore store = getIt.get<FirebaseFirestore>();
+      DocumentReference re =
+          await store.collection("Restraunts").add(r.toMap());
+      return right(1);
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, int>> update(Restraunt r) {
-    // TODO: implement update
+  Future<Either<Failure, int>> update(Restraunt r)  {
     throw UnimplementedError();
+   
   }
-  
+
+  @override
+  Future<Either<Failure, Restraunt>> getRestruntbyUserID(String userid) async {
+    try {
+      FirebaseFirestore store = getIt.get<FirebaseFirestore>();
+      QuerySnapshot<Map<String, dynamic>> s = await store
+          .collection("Restraunts")
+          .where("userID", isEqualTo: userid)
+          .get();
+      if (s.docs.isEmpty) {
+        // todo :select spcefic Failure
+        return left(Failure(""));
+      } else {
+        Restraunt r = Restraunt.fromMap(s.docs.first.data());
+        return right(r);
+      }
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
 }
