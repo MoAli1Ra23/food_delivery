36965de9388576b3d51970934d8959215c613d16
Muhammad Rashad <admoali123@gmail.com>
2024-01-11 09:23:08 +0200

    update menu items repo

--- lib/features/menu/data/repositories/menu_item_repo_impl.dart
+++ lib/features/menu/data/repositories/menu_item_repo_impl.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+  
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 
@@ -9,10 +9,11 @@ import 'package:food_delivery/shared/error/failuer.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../../injection.dart';
+import '../../../../shared/overapp/fire_store.dart';
 import '../../../../shared/overapp/image_uploeder.dart';
 import '../../doamain/repositories/menu_items_repo.dart';
 
-@LazySingleton(as: MenuitemsRepo, env: ['prod', 'debuge'])
+@LazySingleton(as: MenuitemsRepo, env: ['prod', 'debug'])
 class MenuItemRepoImpl extends MenuitemsRepo {
   @override
   Future<Either<Failure, Unit>> deleteItem(MenuItem item, String restruntId) {
@@ -23,26 +24,35 @@ class MenuItemRepoImpl extends MenuitemsRepo {
   Future<Either<Failure, MenuItem>> fetchItemsByMenuId(String menuId) {
     throw UnimplementedError();
   }
-
+  /// تدخل   عناصر  الي 
+  /// 
+  /// 
+  /// itams
+  /// Throw[Failure] if failu
+  
   @override
   Future<Either<Failure, Unit>> insertItems(
-      List<MenuItem> items, String restruntId) async {
-    var x = getIt.get<FirebaseFirestore>();
-    var restrauntref = x.collection('Restraunts').doc(restruntId);
-    DocumentSnapshot restraunt = await restrauntref.get();
-    if (!restraunt.exists) {
-      return left(Failure(""));
-      //TODO :create Failure for not exits state
-    } else {
+      List<MenuItem> items) async {
+    // var fire = getIt.get<FirebaseFirestore>();
+    // var restrauntref = fire.collection('Restraunts').doc();
+    // DocumentSnapshot restraunt = await restrauntref.get();
+    
       try {
+      print('++++++++++++++++++++++++++++++');
+      print('+++++++++Start loop+++++++++++');
+      print('++++++++++++++++++++++++++++++');
         for (var ee in items) {
-          await restrauntref.collection("itams").add(ee.toMap());
+          await insertItemInACollectinio(ee.toMap(),"itams").then((value) => print(value));
+          // await fire.collection("itams").add(ee.toMap()).then((value) => 
+          // print(value));
         }
       } catch (e) {
+        print(e.toString());
         return left(Failure(""));
+
       }
       return right(unit);
-    }
+    
   }
 
   @override
@@ -51,8 +61,31 @@ class MenuItemRepoImpl extends MenuitemsRepo {
     throw UnimplementedError();
   }
 
+///upload image to 
   @override
   Future<Either<Failure, String>> uplodeImage(File item) {
     return uplodeImageAndGetURL(file: item, floder: "image/food");
+  }
+
+  @override
+  Future<Either<Failure, List<MenuItem>>> fetchItems() async {
+    try {
+      List<MenuItem> mil = [];
+      var x = getIt.get<FirebaseFirestore>();
+   var re =
+          await x.collection("itams").get().then((value) => value);
+      if (re.docs.isEmpty) {
+        print("np ele");
+        return right([]);
+      }
+      for (var element in re.docs.toList()) {
+        mil.add(MenuItem.fromMap(element.data()));
+        print(element);
+      }
+      return Right(mil);
+    } catch (e) {
+      print(e);
+      return Left(Failure(e.toString()));
+    }
   }
 }
--- lib/features/menu/doamain/repositories/menu_items_repo.dart
+++ lib/features/menu/doamain/repositories/menu_items_repo.dart
@@ -7,9 +7,10 @@ import '../entiteis/menu_itames.dart';
 
 abstract class MenuitemsRepo {
   Future<Either<Failure, MenuItem>> fetchItemsByMenuId(String menuId);
+  Future<Either<Failure, List<MenuItem>>> fetchItems( );
 
   Future<Either<Failure, Unit>> insertItems(
-      List<MenuItem> items, String restruntId);
+      List<MenuItem> items);
   Future<Either<Failure, Unit>> updateItem(MenuItem item, String restruntId);
   Future<Either<Failure, Unit>> deleteItem(MenuItem item, String restruntId);
   Future<Either<Failure, String>> uplodeImage(File item );
