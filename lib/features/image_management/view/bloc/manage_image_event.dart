772d2cdeae17a1718f1f591b7cf0949467165054
Muhammad Rashad <admoali123@gmail.com>
2024-01-17 17:13:17 +0200

    make ImageSource nullae and set defulte value

--- lib/features/image_management/view/bloc/manage_image_event.dart
+++ lib/features/image_management/view/bloc/manage_image_event.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+ 
 part of 'manage_image_bloc.dart';
 
 sealed class ManageImageEvent extends Equatable {
   const ManageImageEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 /// For new Image to upload it
@@ -23,15 +24,31 @@ class ImageSettingEvent extends ManageImageEvent {
   ///
   final String collction;
   final Image tempImage;
-  final ImageSource imageSorce;
+  final ImageSource? imageSorce;
   final String? url;
   const ImageSettingEvent({
     required this.collction,
     required this.tempImage,
-    required this.imageSorce,
+      this.imageSorce,
       this.url,
   });
-}
+
+  ImageSettingEvent copyWith({
+    String? collction,
+    Image? tempImage,
+    ImageSource? imageSorce,
+    String? url,
+  }) {
+    return ImageSettingEvent(
+      collction: collction ?? this.collction,
+      tempImage: tempImage ?? this.tempImage,
+      imageSorce: imageSorce ?? this.imageSorce,
+      url: url ?? this.url,
+    );
+  }
+  @override
+   List<Object?> get props => [collction,tempImage,imageSorce,url];
+ }
 
 ///  ImageUrlSetEvent set for init Bloc and  Show the Image
 /// if image is not cashed it will cash it
