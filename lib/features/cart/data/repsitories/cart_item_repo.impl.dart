b3246b5e6ad5089a63d642552a1e6a2c1d092641
Muhammad Rashad <admoali123@gmail.com>
2024-01-22 20:05:11 +0200

    refactor it

--- lib/features/cart/data/repsitories/cart_item_repo.impl.dart
+++ lib/features/cart/data/repsitories/cart_item_repo.impl.dart
@@ -19,89 +19,116 @@ class CartItemRepoImpl extends CartItemRepo {
 
   @override
   Future<Either<Failure, List<CartItem>>> fetchOrderList(String ordrtID) async {
-    if (true) {
-      return right(fake);
+    try {
+      var store = getIt.get<FirebaseFirestore>();
+      var x = await store
+          .collection("orders")
+          .doc(ordrtID)
+          .collection("cart")
+          .get()
+          .then((value) => value);
+      List<CartItem> h = [];
+      for (var d in x.docs) {
+        h.add(CartItem.fromMap(d.data()));
+      }
+      return right(h);
+    } catch (e) {
+      return left(Failure(""));
     }
   }
 
   @override
   Future<Either<Failure, String>> updateCartItem(
-      CartItem cartItem, String ordrid) {
-    throw UnimplementedError();
+      CartItem cartItem, String ordrid) async {
+    try {
+      var store = getIt.get<FirebaseFirestore>();
+      var sup = store.collection("orders").doc(ordrid).collection("cart");
+      var x = await sup.where("itemName", isEqualTo: cartItem.itemName).get();
+      if (x.docs.isNotEmpty) {
+        return await sup
+            .doc(x.docs.first.id)
+            .update(cartItem.toMap())
+            .then((value) => right(""));
+      } else {
+        return left(Failure(""));
+      }
+    } catch (e) {
+      return left(Failure(""));
+    }
   }
 
-  List<CartItem> fake = [
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-    const CartItem(
-        itemName: "aaa",
-        menuItemId: "menuItemId",
-        imgUrl: "",
-        price: 25,
-        totalprice: 150,
-        count: 6),
-  ];
+  // List<CartItem> fake = [
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  //   const CartItem(
+  //       itemName: "aaa",
+  //       menuItemId: "menuItemId",
+  //       imgUrl: "",
+  //       price: 25,
+  //       totalprice: 150,
+  //       count: 6),
+  // ];
 
   @override
   Future<Either<Failure, String>> addCartItem(
